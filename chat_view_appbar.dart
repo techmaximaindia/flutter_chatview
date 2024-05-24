171dcd3c353cfb2f5f0c87cb504885319a6a23bb
Vignesh Annamalai <vignesh@annap.in>
2024-05-24 14:52:56 +0530

    Add files via upload

---
+++ chat_view_appbar.dart
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) 2022 Simform Solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import 'dart:io' if (kIsWeb) 'dart:html';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:flutter/material.dart';
+
+import '../values/typedefs.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ChatViewAppBar extends StatelessWidget {
+  const ChatViewAppBar({
+    Key? key,
+    required this.chatTitle,
+    this.backGroundColor,
+    this.userStatus,
+    this.profilePicture,
+    this.chatTitleTextStyle,
+    this.userStatusTextStyle,
+    this.backArrowColor,
+    this.actions,
+    this.elevation,
+    this.onBackPress,
+    this.padding,
+    this.leading,
+    this.showLeading = true,
+    this.platform
+  }) : super(key: key);
+
+  /// Allow user to change colour of appbar.
+  final Color? backGroundColor;
+
+  /// Allow user to change title of appbar.
+  final String chatTitle;
+
+  /// Allow user to change whether user is available or offline.
+  final String? userStatus;
+
+  /// Allow user to change profile picture in appbar.
+  final String? profilePicture;
+
+  /// Allow user to change text style of chat title.
+  final TextStyle? chatTitleTextStyle;
+
+  /// Allow user to change text style of user status.
+  final TextStyle? userStatusTextStyle;
+
+  /// Allow user to change back arrow colour.
+  final Color? backArrowColor;
+
+  /// Allow user to add actions widget in right side of appbar.
+  final List<Widget>? actions;
+
+  /// Allow user to change elevation of appbar.
+  final double? elevation;
+
+  /// Provides callback when user tap on back arrow.
+  final VoidCallBack? onBackPress;
+
+  /// Allow user to change padding in appbar.
+  final EdgeInsets? padding;
+
+  /// Allow user to change leading icon of appbar.
+  final Widget? leading;
+
+  /// Allow user to turn on/off leading icon.
+  final bool showLeading;
+
+  final String? platform;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: elevation ?? 1,
+      child: Container(
+        padding: padding ??
+            EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top,
+              bottom: 4,
+            ),
+        color: backGroundColor ?? Colors.white,
+        child: Row(
+          children: [
+            if (showLeading)
+              leading ??
+                  IconButton(
+                    onPressed: onBackPress ?? () => Navigator.pop(context),
+                    icon: Icon(
+                      (!kIsWeb && Platform.isIOS)
+                          ? Icons.arrow_back_ios
+                          : Icons.arrow_back,
+                      color: backArrowColor,
+                    ),
+                  ),
+            Expanded(
+              child: Row(
+                children: [
+                  if (profilePicture != null)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Stack(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            backgroundImage: NetworkImage(profilePicture!),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: ClipOval(
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                radius: 9,
+                                child: get_platform_widget(platform),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        chatTitle,
+                        style: chatTitleTextStyle ??
+                            const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0.25,
+                            ),
+                      ),
+                      if (userStatus != null)
+                        Text(
+                          userStatus!,
+                          style: userStatusTextStyle,
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            if (actions != null) ...actions!,
+          ],
+        ),
+      ),
+    );
+  }
+  get_platform_widget(platform) 
+  {
+    if (platform == 'livechatwidget') 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.message,
+        size: 13,
+        color: Colors.blueAccent,
+      );
+    } 
+    else if (platform == 'fb_whatsapp') 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.whatsapp,
+        size: 13,
+        color: Colors.green,
+      );
+    } 
+    else if (platform == "telegram") 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.telegram, 
+        size: 13, 
+        color: Colors.blue
+      );
+    } 
+    else if (platform == "facebook") 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.facebook, 
+        size: 13, 
+        color: Colors.blue
+      );
+    } 
+    else if (platform == "twitter") 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.twitter, 
+        size: 13, 
+        color: Colors.blue
+      );
+    } 
+    else if (platform == "wa_whatsapp") 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.whatsapp,
+        size: 13,
+        color: Colors.green,
+      );
+    } 
+    else if (platform == "sms") 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.sms, 
+        size: 13, 
+        color: Colors.blue
+      );
+    } 
+    else if (platform == "instagram") 
+    {
+      return Icon
+      (
+        FontAwesomeIcons.instagram,
+        size: 13, 
+        color: Color.fromARGB(255, 220, 142, 142)
+      );
+    } 
+    else 
+    {
+      return SizedBox.shrink();
+    }
+  }
+}
